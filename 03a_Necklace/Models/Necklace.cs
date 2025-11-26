f900e794137346c7aa871b8a571aedd451f9fdac
Martin <martin_lenart@icloud.com>
2025-11-26 14:07:22 +0100

    update

---
+++ 03a_Necklace/Models/Necklace.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Seido.Utilities.SeedGenerator;
+
+namespace _03a_Necklace.Models
+{
+    #region Necklace using Pearl as a class
+    public class Necklace
+	{
+        public List<Pearl> ListOfPearls { get; set; } = new List<Pearl>();
+        public string Name { get; set; }
+
+        public (Pearl minPearl, Pearl maxPearl) GetMinMaxPearl()
+        {
+            int maxSize = int.MinValue;
+            int minSize = int.MaxValue;
+            Pearl maxPearl = null;
+            Pearl minPearl = null;
+
+            foreach (var item in ListOfPearls)
+            {
+                if (item.Size > maxSize)
+                {
+                    maxSize = item.Size;
+                    maxPearl = item;
+                }
+                if (item.Size < minSize)
+                {
+                    minSize = item.Size;
+                    minPearl = item;
+                }
+            }
+            return (minPearl, maxPearl);
+        }
+        public override string ToString()
+        {
+            string sRet = $"\n{Name}:";
+            foreach (var item in ListOfPearls)
+            {
+                sRet += $"\n{item.ToString()}";
+            }
+            return sRet;
+        }
+
+        public Necklace(int nrPearls, string name)
+		{
+            Name = name;
+            var rnd = new SeedGenerator();
+            for (int i = 0; i < nrPearls; i++)
+            {
+                ListOfPearls.Add(new Pearl(rnd));
+            }
+        }
+        public Necklace(string name, Pearl _p1, Pearl _p2)
+        {
+            Name = name;
+            ListOfPearls.Add(_p1);
+            ListOfPearls.Add(_p2);
+        }
+        public Necklace(Necklace org, string name)
+        {
+            Name = name;
+            ListOfPearls = new List<Pearl>();
+            foreach (var item in org.ListOfPearls)
+            {
+                ListOfPearls.Add(new Pearl(item));
+            }
+        }
+    }
+    #endregion
+}
+
---
+++ 03a_Necklace/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using _03a_Necklace.Models;
+using Seido.Utilities.SeedGenerator;
+
+namespace _03a_Necklace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var rnd = new SeedGenerator();
+        Console.WriteLine("Hello, Perls!");
+
+        var p2 = new Pearl(rnd);
+        Console.WriteLine(p2);
+
+        var n1 = new Necklace(5, "My first necklace");
+        Console.WriteLine(n1);
+
+        var (minPearl, maxPearl) = n1.GetMinMaxPearl();
+        Console.WriteLine($"Smallest pearl: {minPearl}");
+        Console.WriteLine($"Largest pearl: {maxPearl}");        
+    }
+}
+
+
